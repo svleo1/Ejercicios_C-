6c5d4a9a8c7e486e880c870c797eed1a375a31ca
svleo1 <Krasuchileonardo@gmail.com>
2025-04-08 19:59:42 -0300

    Probando....

---
+++ ejercicio_2/ejercicio_2/Program.cs
@@ -0,0 +1,17 @@
+﻿
+/*Nada todavia*/
+
+using System;
+
+namespace ejercicio_2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hola estoy commiteando");
+            Console.ReadKey();
+        }
+    }
+
+}
