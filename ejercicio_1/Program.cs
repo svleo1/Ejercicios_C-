666f552e2582897df6e7719ce06bc12e3e881681
svleo1 <Krasuchileonardo@gmail.com>
2025-04-08 19:25:55 -0300

    Corregido

---
+++ ejercicio_1/Program.cs
@@ -0,0 +1,34 @@
+﻿
+/*Cargar sueldo por hora y horas trabajadas para calcular el salario de un operario*/
+
+using System;
+
+namespace ejercicio_1
+{
+    class Program
+    {
+        public static void Main(string[]args)
+        {
+            /*Definir variables*/
+            int horas;
+            float sueldoPorHora;
+            string linea;
+
+            /*Cargar datos*/
+            Console.WriteLine("Ingrese la cantidad de horas trabajadas: ");
+            linea = Console.ReadLine();
+            horas = int.Parse(linea);
+
+            Console.WriteLine("Ingrese el pago por hora: ");
+            linea = Console.ReadLine();
+            sueldoPorHora = float.Parse(linea);
+
+            /*Mostrar resaultado*/
+            Console.WriteLine("El salario del operario es: ");
+            Console.Write(horas * sueldoPorHora);
+            Console.ReadKey();
+
+        }
+    }
+} 
+        
