2e4581de5ec3e488012dbb1afe739118eb0cde77
svleo1 <Krasuchileonardo@gmail.com>
2025-04-08 22:41:50 -0300

    Hecho...

---
+++ ejercicio_3/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Uso de condicionales basicos: ingresar un numero positivo de uno o dos digitos, mostrar si el numero es de uno o dos digitos*/
+
+using System;
+
+namespace ejercio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Definir variables*/
+            int numeroIngresado;
+            string linea;
+
+            /*Ingresar numero y almacenar*/
+            Console.WriteLine("Ingrese un numero positivo de uno o dos digitos:");
+            linea = Console.ReadLine();
+            numeroIngresado = int.Parse(linea);
+
+            /*Filtrar y mostrar*/
+            if (numeroIngresado <= 0 || numeroIngresado > 99)
+            {
+                Console.WriteLine("El numero debe ser mayor a 0");
+            }
+            else if (numeroIngresado < 10)
+            {
+                Console.WriteLine("El numero tiene un digito");
+            }
+            else
+            {
+                Console.WriteLine("El numero tiene dos digitos");
+            }
+            Console.WriteLine("Presione cualquier tecla para cerrar");
+            Console.ReadKey();
+        }   
+    }
+}
